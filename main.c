14dc7c76464e6937d794aee30b83260da5204c60
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-03 23:52:25 -0600

    Output topic wrapped in quotes if a single argument is provided and nothing if it's inherent with the topic

--- main.c
+++ main.c
@@ -1,3 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char ws = ' ';
+
+const char prefix = '-';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc > 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	char* topic = "it";
+
+	char* noun = topic;
+
+	if (argc == 1 + 1) {
+		char* buff = malloc(strlen(argv[1]) + 2);
+		sprintf(buff, "\"%s\"", argv[1]);
+
+		noun = buff;
+	}
+
+	printf("Look into %s", noun);
+
 	return 0;
 }
